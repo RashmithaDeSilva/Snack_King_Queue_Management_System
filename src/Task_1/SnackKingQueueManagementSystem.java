b85f3bce6c023ea07e4784a7ebf53597c1cfbfb8
Lahiru De Silva <lahirurashmitha00@gmail.com>
2023-08-26 17:28:59 +0530

    Remove a customer from a queue (103 or RCQ)

--- src/Task_1/SnackKingQueueManagementSystem.java
+++ src/Task_1/SnackKingQueueManagementSystem.java
@@ -7,6 +7,7 @@ public class SnackKingQueueManagementSystem {
     private boolean[] q1 = {false, false};
     private boolean[] q2 = {false, false, false};
     private boolean[] q3 = {false, false, false, false, false};
+    private int pizzaCount = 100;
 
     // Main method -----------------------------------------------------------------------------------------------------
     public static void main(String[] args) {
@@ -26,7 +27,7 @@ public class SnackKingQueueManagementSystem {
                     SKQMS_obj.addCustomer();
                     break;
                 case "103", "RCQ":
-                    System.out.println("103");
+                    SKQMS_obj.removeCustometr();
                     break;
                 case "104", "PCQ":
                     System.out.println("104");
@@ -106,20 +107,20 @@ public class SnackKingQueueManagementSystem {
     }
 
 
-    // Get int user input -------------------------------------------------------------------------------------------
+    // Get int user input ----------------------------------------------------------------------------------------------
     private int getIntInput(String inputLabelName) {
         System.out.print(inputLabelName);
         Scanner scanner = new Scanner(System.in);
         try {
             return scanner.nextInt();
         } catch (Exception e) {
-            System.out.println("Invalid input. Please enter an integer !");
+            System.out.println("\tInvalid input. Please enter an integer !");
             return -1;
         }
     }
 
 
-    // View all queues (100 or VFQ)
+    // View all queues (100 or VFQ) ------------------------------------------------------------------------------------
     private void viewAllQueues() {
         printWindowName("View All Queues ");
         System.out.println("\t\t\t\t\t\t\tQ1\tQ2\tQ3");
@@ -145,7 +146,7 @@ public class SnackKingQueueManagementSystem {
     }
 
 
-    // View all empty queues (101 or VFQ)
+    // View all empty queues (101 or VFQ) ------------------------------------------------------------------------------
     private void viewAllEmptyQueues() {
         printWindowName("View All Empty Queues ");
         System.out.print("\t\t\tQueues 1:\t");
@@ -170,7 +171,7 @@ public class SnackKingQueueManagementSystem {
     }
 
 
-    // Add customer to a queue (102 or ACQ)
+    // Add customer to a queue (102 or ACQ) ----------------------------------------------------------------------------
     private void addCustomer() {
         printWindowName("Add Customer to a Queue ");
         boolean loopbreack = true;
@@ -231,5 +232,91 @@ public class SnackKingQueueManagementSystem {
             }
         }
 
+    }
+
+
+    // Remove a customer from a queue (103 or RCQ) ---------------------------------------------------------------------
+    private void removeCustometr() {
+        printWindowName("Remove a Customer from a Queue");
+        boolean loopbreack = true;
+
+        while (loopbreack) {
+            int queueNumber = getIntInput("\nEnter queue number:\t");
+            int customerNumber = -1;
+            if (queueNumber > 0 && queueNumber < 4) {
+                customerNumber = getIntInput("Enter customer number:\t");
+            }
+
+            if (queueNumber == 1) {
+                if (customerNumber == -1) {
+                } else if ((customerNumber-1) > q1.length || 0 > (customerNumber-1)) {
+                    System.out.println("\tInvalid customer number !\n");
+                } else {
+                    if (!q1[customerNumber-1]) {
+                        System.out.println("\tThis position is already empty");
+
+                    } else {
+                        for (int i=0;i<q1.length;i++) {
+                            if (i == (customerNumber-1)) {
+                                q1[i] = false;
+
+                            } else if (i < (q1.length-1) && 0 < i && q1[i] && !q1[i-1]) {
+                                q1[i-1] = q1[i];
+                            }
+                        }
+                        q1[q1.length-1] = false;
+                    }
+                    loopbreack = false;
+                }
+
+            } else if (queueNumber == 2) {
+                if (customerNumber == -1) {
+                } else if ((customerNumber-1) > q2.length || 0 > (customerNumber-1)) {
+                    System.out.println("\tInvalid customer number !\n");
+                } else {
+                    if (!q2[customerNumber-1]) {
+                        System.out.println("\tThis position is already empty");
+
+                    } else {
+                        for (int i=0;i<q2.length;i++) {
+                            if (i == (customerNumber-1)) {
+                                q2[i] = false;
+
+                            } else if (i < (q2.length-1) && 0 < i && q2[i] && !q2[i-1]) {
+                                q2[i-1] = q2[i];
+                            }
+                        }
+                        q2[q2.length-1] = false;
+                    }
+                    loopbreack = false;
+                }
+
+            } else if (queueNumber == 3) {
+                if (customerNumber == -1) {
+                } else if ((customerNumber-1) > q3.length || 0 > (customerNumber-1)) {
+                    System.out.println("\tInvalid customer number !\n");
+                } else {
+                    if (!q3[customerNumber-1]) {
+                        System.out.println("\tThis position is already empty");
+
+                    } else {
+                        for (int i=0;i<q3.length;i++) {
+                            if (i == (customerNumber-1)) {
+                                q3[i] = false;
+
+                            } else if (i < (q3.length-1) && 0 < i && q3[i] && !q3[i-1]) {
+                                q3[i-1] = q3[i];
+                            }
+                        }
+                        q3[q3.length-1] = false;
+                    }
+                    loopbreack = false;
+                }
+
+            } else if (queueNumber == -1) {
+            } else {
+                System.out.println("Invalid queue number !");
+            }
+        }
     }
 }
